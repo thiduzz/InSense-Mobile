a9aac64f55039d6ad970af05488cdc757f3e2fa4
thiduzz <thiduzz14@gmail.com>
2017-06-20 01:04:58 +0200

    Adicionado o mapa do Google Maps

--- app/src/main/java/com/apackage/insense/MapFragment.java
+++ app/src/main/java/com/apackage/insense/MapFragment.java
@@ -1,19 +1,41 @@
 package com.apackage.insense;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.apackage.api.ServerConnection;
 import com.apackage.api.ServerConnectionListener;
 import com.apackage.db.DataBase;
+import com.apackage.utils.Constants;
 import com.apackage.utils.OnActivityFragmentsInteractionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Map;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 
 /**
@@ -24,11 +46,16 @@ import java.util.Map;
  * Use the {@link MapFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapFragment extends Fragment implements ServerConnectionListener {
+public class MapFragment extends Fragment implements ServerConnectionListener, OnMapReadyCallback, LocationListener {
 
     private OnActivityFragmentsInteractionListener mListener;
     private DataBase db;
     private ServerConnection con;
+    private SupportMapFragment map;
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private MarkerOptions userMarker;
 
     public MapFragment() {
         // Required empty public constructor
@@ -42,13 +69,22 @@ public class MapFragment extends Fragment implements ServerConnectionListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, Constants.PERMISSION_RC_MAPS);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container,
+                false);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -93,5 +129,100 @@ public class MapFragment extends Fragment implements ServerConnectionListener {
     @Override
     public void onConnectionSuccess(Map<String, Object> result) {
 
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMyLocationEnabled(true);
+        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+
+        if(ContextCompat.checkSelfPermission
+                (getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission
+                (getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
+            Location location =
+                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+            if(location != null){
+                onLocationChanged(location);
+            }
+        }
+        else{
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    1000
+            );
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        LatLng userLocation = new LatLng(location.getLatitude()
+                ,location.getLongitude());
+
+        if(userMarker == null){
+            userMarker = new MarkerOptions();
+            userMarker.title("Eu");
+            userMarker.position(userLocation);
+
+            mMap.addMarker(userMarker);
+        }
+        else {
+            userMarker.position(userLocation);
+        }
+
+        mMap.moveCamera(
+                CameraUpdateFactory
+                        .newLatLngZoom(userLocation,15));
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 1000 &&
+                grantResults.length > 0 &&
+                grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED){
+
+            if(ContextCompat.checkSelfPermission
+                    (getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+        }
     }
 }
--- app/src/main/java/com/apackage/utils/Constants.java
+++ app/src/main/java/com/apackage/utils/Constants.java
@@ -5,9 +5,13 @@ package com.apackage.utils;
  */
 
 public class Constants {
+    //Chamadas
     public static final String REQUEST_LOGIN = "login_request";
     public static final String REQUEST_SETTINGS = "settings_request";
     public static final String REQUEST_DEVICES = "devices_request";
     public static final String REQUEST_REFRESH_TOKEN = "token_request";
     public static final String REQUEST_VALIDATE_TOKEN = "validate_token";
+
+    //Request codes
+    public static final int PERMISSION_RC_MAPS = 405;
 }
